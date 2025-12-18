68d85244807050e188a05d4d952728aba0c5dfed
hollali <dheztinykartel@gmail.com>
2025-12-18 16:29:08 +0000

    first commit

---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parliamentary Service of Ghana Inventory Management System </title>
+    <link rel="stylesheet" href="./css/login.css ">
+</head>
+
+<body>
+    <div class="container">
+        <div class="loginHeader">
+            <h1>Parliamentary Service of Ghana</h1>
+            <h3>Inventory Management System </h3>
+        </div>
+        <div class="loginBody">
+            <form action="">
+                <div class="loginInputsContainer">
+                    <label for="username">Username</label>
+                    <input placeholder="Enter Your Username" type="text" name="username" required="required"
+                        autocomplete="off">
+                </div>
+                <div class="loginInputsContainer">
+                    <label for="">Password</label>
+                    <input placeholder="Enter Your Password" type="password" name="password" required="required"
+                        autocomplete="off">
+                </div>
+                <div class="loginButtonContainer">
+                    <button type="submit">LOGIN</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ pages/dashboard.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parliamentary Service of Ghana Inventory Management System Admin Dashboard</title>
+</head>
+
+<body>
+    <div>
+        <div class="dashboard_sidebar">
+            <h3 class="dashboard_logo">PIMS</h3>
+            <div class="dashboard_sidebar_user">
+                <img src="" alt="">
+            </div>
+        </div>
+        <div class="dashboard_content"></div>
+    </div>
+</body>
+
+</html>
