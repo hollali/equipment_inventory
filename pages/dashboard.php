4adfab6b324e1f8b761750ab0ff17ceb994c7ef8
hollali <dheztinykartel@gmail.com>
2025-12-19 10:11:40 +0000

    `Update dashboard.php: change CSS link path and add logout link`

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parliamentary Service of Ghana Inventory Management System Admin Dashboard</title>
-    <link rel="stylesheet" href="./css/dashboard.css">
+    <link rel="stylesheet" href="../css/dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
 
 </head>
 
 <body>
-    <div>
+    <div class="dashboardMainContainer">
         <div class="dashboard_sidebar">
             <h3 class="dashboard_logo">PIMS</h3>
             <div class="dashboard_sidebar_user">
@@ -33,7 +33,8 @@
         </div>
         <div class="dashboard_content">
             <div class="dashboard_content_topNav">
-                <i class="fa-solid fa-bars"></i>
+                <a href=""><i class="fa-solid fa-bars"></i></a>
+                <a href=""><i class="fa-solid fa-power-off">Logout</i></a>
             </div>
         </div>
     </div>
