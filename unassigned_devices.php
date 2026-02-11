0ef7d0cddb911713a83df277a9ea1255b8b3f9ae
hollali <dheztinykartel@gmail.com>
2026-02-11 02:31:14 +0000

    `Added JavaScript libraries and footer to unassigned_devices.php`

--- unassigned_devices.php
+++ unassigned_devices.php
@@ -1192,6 +1192,7 @@ function getConditionDisplay($condition, $conditionColors, $conditionLabels)
     </div>
     <!--footer-->
     <?php include __DIR__ . '/footer.php'; ?>
+
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
